9acd42636c5bff8cc573064c459d367dae724e0d
Phương Linh <88975081+ptpl2602@users.noreply.github.com>
2023-06-02 11:13:58 +0700

    Điểm danh 2/6/2023

--- BIGSCHOOL/Models/ApplicationDBContext.cs
+++ BIGSCHOOL/Models/ApplicationDBContext.cs
@@ -11,6 +11,8 @@ namespace BIGSCHOOL.Models
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -19,6 +21,26 @@ namespace BIGSCHOOL.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modeBuilder)
+        {
+            modeBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modeBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modeBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modeBuilder);
         }
     }
 }
